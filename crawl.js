c9a1e642192d4269706afa2da1a91f7fb3e4dea9
oguzhan <oguzhanbalta99@gmail.com>
2022-10-17 01:01:31 +0300

    update

--- crawl.js
+++ crawl.js
@@ -1,60 +1,59 @@
 import axios from "axios";
 import cheerio from "cheerio";
-let page_number = 1;
-const data_offset = 4;
-const domain="https://oa01.widget.ega.eu"
-const url = `https://oa01.widget.ega.eu/widget/fahrzeuge/alle/module/car/controller/index/seite/${page_number}?bot=iamnotabot&dontCompress=1&offset=${data_offset}&resultorder=PreisLaden&resultdirection=ASC`;
+import fs from 'fs';
 
 
+function run(page_number) {
+    const domain = 'https://oa01.widget.ega.eu';
+    const data_offset = 1;
+    const url = `https://oa01.widget.ega.eu/widget/fahrzeuge/alle/module/car/controller/index/seite/${page_number}?bot=iamnotabot&dontCompress=1&offset=${data_offset}&resultorder=PreisLaden&resultdirection=ASC`;
+    fetchData(url).then( (res) => {
 
-fetchData(url).then( (res) => {
-
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const carPrice = $('.search-result-vehicle-box>.vehicle-price-container>.price');
-    // carPrice.each(function() {
-    //     let price = $(this).find('a').text().trim();
-    //     console.log('carPrice:', price);
-    // });
-
-    const carImage = $('.search-result-vehicle-box>.vehicle-image-container>a>.vehicle-image-wrap>img');
-
-
-    const carName = $('.vehicle-title>a .brand-and-model');
-    const carSubtitle = $('.search-result-vehicle-box>.vehicle-info-container .model-zusatz');
-    const carModelYear = $('.vehicle-title>a>div>span.first-registration');
-    const carDistance = $('.vehicle-title>a>div>span.mileage');
-    const carPetrol = $('.vehicle-title>a>div>span.petrol');
-    const carPower = $('.vehicle-title>a>div>span.power');
-    const carGear = $('.vehicle-title>a>div>span.gear');
-    const carColor = $('.vehicle-title>a>div>span.color');
-    const carConsumption = $('.search-result-vehicle-box>.vehicle-info-container>.vehicle-info>.row:nth-child(1)>div>.value');
-    const carCO2 = $('.search-result-vehicle-box>.vehicle-info-container>.vehicle-info>.row:nth-child(2)>div>.value');
+        const html = res.data;
+        const $ = cheerio.load(html);
+        const carPrice = $('.search-result-vehicle-box>.vehicle-price-container>.price');
+        const carImage = $('.search-result-vehicle-box>.vehicle-image-container>a>.vehicle-image-wrap>img');
+        const carName = $('.vehicle-title>a .brand-and-model');
+        const carSubtitle = $('.search-result-vehicle-box>.vehicle-info-container .model-zusatz');
+        const carModelYear = $('.vehicle-title>a>div>span.first-registration');
+        const carDistance = $('.vehicle-title>a>div>span.mileage');
+        const carPetrol = $('.vehicle-title>a>div>span.petrol');
+        const carPower = $('.vehicle-title>a>div>span.power');
+        const carGear = $('.vehicle-title>a>div>span.gear');
+        const carColor = $('.vehicle-title>a>div>span.color');
+        const carConsumption = $('.search-result-vehicle-box>.vehicle-info-container>.vehicle-info>.row:nth-child(1)>div>.value');
+        const carCO2 = $('.search-result-vehicle-box>.vehicle-info-container>.vehicle-info>.row:nth-child(2)>div>.value');
 
 
-    for (let i = 0; i < carPrice.length; i++) {
-        const car = {
-            'title': carName[i]?.children[0].data.trim(),
-            'photos': [domain+carImage[i]?.attribs['src']],
-            'brand': carName[i]?.children[0].data.trim(),
-            'model': carSubtitle[i]?.children[0].data.trim(),
-            'year': carModelYear[i]?.children[0].data.trim(),
-            'kilometers': carDistance[i]?.children[0].data.trim(),
-            'price': carPrice[i]?.children[0].data.trim(),
-            'description': "",
-            'location': "",
-            'petrol': carPetrol[i]?.children[0].data.trim().split(" |")[0],
-            'power': carPower[i]?.children[0].data.trim().split(" |")[0],
-            'gear': carGear[i]?.children[0].data.trim(),
-            'color': carColor[i]?.children[0].data.trim().split("Farbe: ")[1],
-            'intercity_fuel_consumption': carConsumption[i]?.children[0].data.split("l/100km")[0].trim(),
-            'avarage_fuel_consumption': '',
-            'carbon_dioxide_emission': carCO2[i]?.children[0].data.split("g/km")[0].trim(),
-            'carbon_dioxide_efficiency': "",
+        for (let i = 0; i < data_offset; i++) {
+            const car = {
+                'title': carName[i]?.children[0].data.trim() || "",
+                'photos': [domain+carImage[i]?.attribs['src']],
+                'brand': carName[i]?.children[0].data.trim() || "",
+                'model': carSubtitle[i]?.children[0].data.trim() || "",
+                'year': carModelYear[i]?.children[0].data.trim() || "",
+                'kilometers': carDistance[i]?.children[0].data.trim() || "",
+                'price': carPrice[i]?.children[0].data.trim() || "",
+                'description': "",
+                'location': "",
+                'petrol': carPetrol[i]?.children[0].data.trim().split(" |")[0] || "",
+                'power': carPower[i]?.children[0].data.trim().split(" |")[0] || "",
+                'gear': carGear[i]?.children[0].data.trim(),
+                'color': carColor[i]?.children[0].data.trim().split("Farbe: ")[1] || "",
+                'intercity_fuel_consumption': carConsumption[i]?.children[0].data.split("l/100km")[0].trim() || "",
+                'avarage_fuel_consumption': "",
+                'carbon_dioxide_emission': carCO2[i]?.children[0].data.split("g/km")[0].trim() || "",
+                'carbon_dioxide_efficiency': "",
+            };
+
+
+            fs.appendFile('data.json', JSON.stringify(car) + ',', function (err) {
+                if (err) throw err;
+                console.log('Saved!' + i);
+            });
         };
-        console.log(car);
-    };
-})
+    })
+}
 
 
 async function fetchData(url){
@@ -67,5 +66,10 @@ async function fetchData(url){
         return;
     }
     return response;
+}
+
+
+for (let i = 0; i < 100; i++) {
+    run(i+1);
 }
 
